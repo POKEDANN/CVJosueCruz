7cee4a95b394e73d0b7a2ab133feeaf7f92a0ca6
Daniel <Pokedann@MacBook-Pro-de-Daniel.local>
2016-08-16 22:47:24 -0500

    charts terminados, empieza sección de reconocimientos y cursos

--- js/danielcv.js
+++ js/danielcv.js
@@ -1,3 +1,42 @@
 var filterizd = $('.filtr-container').filterizr({
    //options object
 });
+
+$( document ).ready(function() {
+            $(".test-circle").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 60,
+        });
+            $(".test-circle1").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 60,
+        });
+            $(".test-circle2").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 30,
+        });
+            $(".test-circle3").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 30,
+        });
+            $(".test-circle4").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 20,
+        });
+            $(".test-circle5").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 40,
+        });
+        });
