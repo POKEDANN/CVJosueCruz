2149acf6ba61c881c76649524f07e05b62b99012
Daniel <Pokedann@MacBook-Pro-de-Daniel.local>
2017-06-30 21:17:15 -0500

    Cambios responsive finales, continuación MDB
    Se dejo el botón del modal del CV para medidas lg, se creo un botón
    para descargar CV para medidas md para abajo, se quito el plugin scroll
    reveal que ni siquiera se esta usando, detalles finales en la sección
    de ingles y escolaridad.
    En MDB se actualizo el portafolio, se quito lo de OWL, se modifico la
    sección de actualización y se metieron las fotos en un acordeón, se
    agrego una sección con un Timeline, solo falta modificarlo según las
    necesidades.

--- public/myCVs/CV-MDB/js/danielcvmdb.js
+++ public/myCVs/CV-MDB/js/danielcvmdb.js
@@ -22,4 +22,12 @@ $( document ).ready(function() {
     $(".rotate-btn.back").click(function(){
     $("#card-1").removeClass("flipped");
 });
+    $('.collapse.in').prev('.panel-heading').addClass('active');
+  $('#accordion, #bs-collapse')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
 });
